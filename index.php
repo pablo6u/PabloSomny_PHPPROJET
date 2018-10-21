61de7e2f20517f63d9c859e31f4fcd850672f62f
Benjamin PABLO <benjamin_pablo@hotmail.fr>
2018-10-21 14:13:17 +0200

    1er commit 14h13

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Accueil</title>
+        <link rel="stylesheet" media="screen and (min-width:721px)" href="style.css">
+        <link rel="stylesheet" media="screen and (max-width:720px)" href="mobile.css">
+        <meta name="viewport" content="width=max-device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <p class="Info_Projet">Projet ProgWeb 2A</p>
+        <header class="Selec_JOH">
+            <h1>Accueil</h1>  
+            <nav>
+                <a href="JeuxOlympiques.html">Retourner à la sélection de période</a>
+                <a href="Index.html">Retourner à la page principale</a>
+            </nav>
+        </header>
+    </body>
+</html>
