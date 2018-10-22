7f999cab20b4696da4ae545a270857a7e26c3227
Benjamin PABLO <benjamin_pablo@hotmail.fr>
2018-10-22 19:28:42 +0200

    123

--- acceuil.php
+++ acceuil.php
@@ -4,11 +4,10 @@
         <title>Accueil</title>
         <link rel="stylesheet" media="screen and (min-width:721px)" href="style.css">
         <link rel="stylesheet" media="screen and (max-width:720px)" href="mobile.css">
-        <!--<meta name="viewport" content="width=max-device-width, initial-scale=1">-->
-
+        <meta name="viewport" content="width=max-device-width, initial-scale=1">
     </head>
 
-    <body class="class1">
+    <body>
         <p>Projet ProgWeb 2A</p>
         <header>
             <h1>Accueil</h1>
@@ -17,7 +16,6 @@
                 <a href="creerCompte.php">Creer un compte</a>
                 <a href="creerNews.php">Creer une news</a>
                 <a href="identificaton.php">Vous connecter</a>
-
             </nav>
         </header>
     </body>
