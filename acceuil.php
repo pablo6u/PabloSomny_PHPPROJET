601d3507ef98b0cee3321fbb432442f9d1829688
Benjamin PABLO <benjamin_pablo@hotmail.fr>
2018-10-24 18:26:16 +0200

    sqc

--- acceuil.php
+++ acceuil.php
@@ -2,14 +2,14 @@
     <head>
         <meta charset="UTF-8">
         <title>Accueil</title>
-        <link rel="stylesheet" media="screen and (min-width:721px)" href="style.css">
-        <link rel="stylesheet" media="screen and (max-width:720px)" href="mobile.css">
+        <link rel="stylesheet" media="screen" href="style.css">
+        <!--<link rel="stylesheet" media="screen and (max-width:720px)" href="mobile.css">-->
         <meta name="viewport" content="width=max-device-width, initial-scale=1">
     </head>
 
     <body>
         <p>Projet ProgWeb 2A</p>
-        <header>
+        <header class=".header">
             <h1>Accueil</h1>
             <br/>
             <nav>
